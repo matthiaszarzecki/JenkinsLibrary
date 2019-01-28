431bda0ee33361a96b99b3535a2a82f6d62a8433
Zarzecki, Matthias <matthias.zarzecki@aperto.com>
2019-01-28 12:56:22 +0100

    Renamed postHTML function to publishHTML

--- vars/postDocumentation.groovy
+++ vars/postDocumentation.groovy
@@ -1,8 +1,6 @@
 #!/usr/local/bin groovy
 package com.matthiaszarzecki.jenkinsLibrary
 
-import com.matthiaszarzecki.jenkinsLibrary.*
-
 def call(directory, filename) {
-  postHTML('Documentation', directory, filename)
+  publishHTML('Documentation', directory, filename)
 }
