2d7db94962de26715c01cccc595c9104dbe19e0e
Zarzecki, Matthias <matthias.zarzecki@aperto.com>
2019-01-28 11:07:27 +0100

    Added postStage-Function, added package-name

---
+++ vars/postStage.groovy
@@ -0,0 +1,6 @@
+#!/usr/local/bin groovy
+package com.matthiaszarzecki.jenkinsLibrary
+
+def postStage(stageName) {
+  echo "###################### Stage: " + stageName
+}
--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -1,5 +1,5 @@
 #!/usr/local/bin groovy
-package com.matthias
+package com.matthiaszarzecki.jenkinsLibrary
 
 def call() {
   echo "Jenkins Test Function Call Works! :D"
