6cfd598772b75037be2d4663abbe216bfa4f332a
Zarzecki, Matthias <matthias.zarzecki@aperto.com>
2019-01-28 12:04:15 +0100

    Added import to postDocumentation

--- vars/postDocumentation.groovy
+++ vars/postDocumentation.groovy
@@ -1,6 +1,8 @@
 #!/usr/local/bin groovy
 package com.matthiaszarzecki.jenkinsLibrary
 
+import package com.matthiaszarzecki.jenkinsLibrary
+
 def call(directory, filename) {
   postHTML('Documentation', directory, filename)
 }
