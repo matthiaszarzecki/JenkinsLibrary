1d8923f7eddae946b3301bf1cb63d9dde34c6652
Zarzecki, Matthias <matthias.zarzecki@aperto.com>
2019-01-28 14:22:46 +0100

    Fixed faulty postCodeCoverage-Function

--- vars/postCodeCoverageReport.groovy
+++ vars/postCodeCoverageReport.groovy
@@ -2,7 +2,6 @@
 package com.matthiaszarzecki.jenkinsLibrary
 
 def call(directory, filename) {
-  postHTML('Coverage Report', './fastlane/test_output/xcov_report/', 'index.html')
   publishHTML([allowMissing:          false,
                alwaysLinkToLastBuild: false,
                keepAll:               false,
